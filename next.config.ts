54b11e54db4fc26cb00ecba0acbc309daa403058
Hexarislogen-blip <hectorsedo@gmail.com>
2026-02-20 13:45:23 +0100

    revert: simplify next.config.ts to fix build errors

--- next.config.ts
+++ next.config.ts
@@ -2,32 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    optimizeCss: true,
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 244000,
-        cacheGroups: {
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            priority: -10,
-            chunks: 'all',
-          },
-        },
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
